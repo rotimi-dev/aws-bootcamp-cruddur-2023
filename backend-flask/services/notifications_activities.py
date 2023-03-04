f6e02bd6933b9c5be8b5078877586c88ca05158e
Rotimi Bello <rotimisrael@gmail.com>
2023-03-04 14:31:59 +0000

    Update OpenAPI further, made some changes

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta, timezone
-class HomeActivities:
+class NotificationsActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle': 'Timi Israel',
-      'message': 'Cloud is really fun!',
+      'handle':  'spamdam',
+      'message': 'This is in respect of Cipher Pol Aegis 0!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -22,23 +22,5 @@ class HomeActivities:
         'created_at': (now - timedelta(days=2)).isoformat()
       }],
     },
-    {
-      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Worf',
-      'message': 'I am out of prune juice',
-      'created_at': (now - timedelta(days=7)).isoformat(),
-      'expires_at': (now + timedelta(days=9)).isoformat(),
-      'likes': 0,
-      'replies': []
-    },
-    {
-      'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Garek',
-      'message': 'My dear doctor, I am just simple tailor',
-      'created_at': (now - timedelta(hours=1)).isoformat(),
-      'expires_at': (now + timedelta(hours=12)).isoformat(),
-      'likes': 0,
-      'replies': []
-    }
     ]
     return results
